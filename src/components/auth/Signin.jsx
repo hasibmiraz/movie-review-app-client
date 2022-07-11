7eaa773dcad733bf4330775f30f092a029977ddc
hasibmiraz <hasibmiraz09@gmail.com>
2022-07-12 01:47:55 +0600

    Sign in input form completed

--- src/components/auth/Signin.jsx
+++ src/components/auth/Signin.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import Container from '../Container';
+import FormInput from '../form/FormInput';
+import Title from '../form/Title';
 
 const Signin = () => {
-  return <div className="fixed inset-0 -z-10 bg-primary"></div>;
+  return (
+    <div className="fixed inset-0 -z-10 bg-primary flex justify-center items-center">
+      <Container>
+        <form className="bg-secondary rounded p-6 w-72 space-y-6">
+          <Title>Sign in</Title>
+          <FormInput
+            name="email"
+            type="email"
+            placeholder="hasib@email.com"
+            label="Email"
+          />
+          <FormInput
+            name="password"
+            type="password"
+            placeholder="********"
+            label="Password"
+          />
+        </form>
+      </Container>
+    </div>
+  );
 };
 
 export default Signin;
---
+++ src/components/form/FormInput.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const FormInput = ({ name, type, placeholder, label, ...rest }) => {
+  return (
+    <div className="flex flex-col-reverse">
+      <input
+        id={name}
+        type={type}
+        name={name}
+        className="bg-transparent rounded border-2 border-dark-subtle w-full text-lg outline-none focus:border-white p-1 text-white peer"
+        placeholder={placeholder}
+        {...rest}
+      />
+      <label
+        htmlFor={name}
+        className="text-dark-subtle font-semibold peer-focus:text-white transition duration-200 self-start mb-1"
+      >
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default FormInput;
