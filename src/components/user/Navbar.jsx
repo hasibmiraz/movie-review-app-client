6caa6043485cc2831075d56352a39ab53e1b2c28
hasibmiraz <hasibmiraz09@gmail.com>
2022-07-12 00:51:47 +0600

    Navbar in progress

--- src/components/user/Navbar.jsx
+++ src/components/user/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MdOutlineLightMode } from 'react-icons/md';
 
 const Navbar = () => {
   return (
@@ -6,8 +7,20 @@ const Navbar = () => {
       <div className="max-w-screen-xl mx-auto p-2">
         <div className="flex justify-between items-center">
           <img src="logo.png" alt="" className="h-10" />
-          <ul>
-            <li>Login</li>
+          <ul className="flex items-center space-x-3">
+            <li>
+              <button className="bg-dark-subtle p-1 rounded">
+                <MdOutlineLightMode className="text-white" size={24} />
+              </button>
+            </li>
+            <li>
+              <input
+                type="text"
+                className="border-2 border-dark-subtle p-1 bg-transparent rounded text-xl outline-none focus:border-white text-white transition duration-300"
+                placeholder="Search..."
+              />
+            </li>
+            <li className="text-white font-semibold text-lg">Login</li>
           </ul>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#006176',
-        secondary: '#004E70',
-        'dark-primary': '#171717',
-        'dark-secondary': '#272727',
+        primary: '#171717',
+        secondary: '#272727',
         'dark-subtle': 'rgba(255, 255, 255, 0.5)',
-        'light-subtle': 'rgba(0, 97, 118, 0.85)',
+        'light-subtle': 'rgba(39, 39, 39, 0.5)',
       },
     },
   },
