a45251cd2f6d94e82e9ccb2ce152fe893e512e29
hasibmiraz <hasibmiraz09@gmail.com>
2022-07-12 17:39:44 +0600

    OTP form complete

--- src/components/auth/EmailVerification.jsx
+++ src/components/auth/EmailVerification.jsx
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import Container from '../Container';
+import Submit from '../form/Submit';
+import Title from '../form/Title';
 
 const EmailVerification = () => {
-  return <div>email-verification</div>;
+  const otpLength = 6;
+  const [otp, setOtp] = useState(new Array(otpLength).fill(''));
+  const [activeOtpIndex, setActiveOtpIndex] = useState(0);
+  const inputRef = useRef();
+
+  const focusNextInputField = (index) => {
+    setActiveOtpIndex(index + 1);
+  };
+
+  const focusPrevInputField = (index) => {
+    let nextIndex;
+    const diff = index - 1;
+    nextIndex = diff !== 0 ? diff : 0;
+    setActiveOtpIndex(nextIndex);
+  };
+
+  const handleOtpChange = ({ target }, index) => {
+    const { value } = target;
+    const newOtp = [...otp];
+    newOtp[index] = value.substring(value.length - 1, value.length);
+    if (!value) focusPrevInputField(index);
+    else focusNextInputField(index);
+    setOtp([...newOtp]);
+  };
+
+  const handleKeyDown = ({ key }, index) => {
+    if (key === 'Backspace') {
+      focusPrevInputField(index);
+    }
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [activeOtpIndex]);
+
+  return (
+    <div className="fixed inset-0 -z-10 bg-primary flex justify-center items-center">
+      <Container>
+        <form className="bg-secondary rounded p-6 space-y-6">
+          <div>
+            <Title>Please enter the OTP to verify your account</Title>
+            <p className="text-center text-dark-subtle">
+              OTP has been sent to your email
+            </p>
+          </div>
+          <div className="flex justify-between">
+            {otp.map((_, index) => (
+              <input
+                ref={activeOtpIndex === index ? inputRef : null}
+                key={index}
+                type="number"
+                value={otp[index] || ''}
+                onChange={(e) => handleOtpChange(e, index)}
+                onKeyDown={(e) => handleKeyDown(e, index)}
+                className="w-12 h-12 border-2 border-dark-subtle bg-transparent rounded text-white text-center focus:border-white outline-none text-lg font-semibold otpNumber"
+              />
+            ))}
+          </div>
+          <Submit value="Verify Account" />
+        </form>
+      </Container>
+    </div>
+  );
 };
 
 export default EmailVerification;
